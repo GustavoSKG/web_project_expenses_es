e8fe8b8dd23dca03224f0b147569574bc65659d6
Restless Underground <restless.underground@gmail.com>
2026-07-31 01:11:53 -0600

    array declarations

---
+++ scripts/calculations.js
@@ -0,0 +1,15 @@
+
+let budgetValue = 0;
+let totalExpensesValue = 0;
+
+ let expensesEntries = [
+    ["groceries", 33 ],
+    ["restaurants", 50 ],
+    ["transport", 12 ],
+    ["home", 70 ],
+    ["subscriptions", 12 ],
+    ["groceries", 28 ],
+    ["subscriptions", 12 ]
+];
+
+
