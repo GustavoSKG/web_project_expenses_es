492617002eb2ae0e854482bbd5ef4eb86babc4e4
Restless Underground <restless.underground@gmail.com>
2026-08-11 01:21:30 -0600

    step 9

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,5 +1,6 @@
 
 let budgetValue = 0;
+let averageExpenses = 0;
 let totalExpensesValue = 0;
 
 let expenseEntries = [
@@ -12,4 +13,25 @@ let expenseEntries = [
     ["subscriptions", 12 ]
 ];
 
+for(let )
 
+
+function calculateAverageExpense(){
+    if(expenseEntries.length === 0){
+        return 0;
+    } else {
+
+        totalExpensesValue = 0;
+
+    for(let expense of expenseEntries){
+        totalExpensesValue += expense[1];
+        console.log(`Valor total de los gastos: ${totalExpensesValue}`);
+    }
+    }
+         return totalExpensesValue / expenseEntries.length;
+}
+    
+
+function calculateBalance(){
+
+}
